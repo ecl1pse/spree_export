8f3bbf34ae322c0d764437e1179f9e8b57da8fd5
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-07-02 00:28:43 -0700

    Renamed to #export_csv as well as a more maintainable test suite

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,7 @@
 require 'csv'
 Spree::Order.instance_eval do
 
-  def self.generate_csv(options = {})
+  def self.export_csv(options = {})
     CSV.generate(options) do |csv|
       address_column_names = ["firstname", "lastname", "address1", "address2", "city", "zipcode", "phone", "state_name", "alternative_phone", "company"]
       order_column_names = ["id", "number", "total", "state", "shipment_state", "email", "user_id", "completed_at", "payment_state", "special_instructions"]
--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -2,38 +2,37 @@ require 'spec_helper'
 
 describe Spree::Order do
   before do
-    create_list(:completed_order_with_totals, 1)
+    create_list(:completed_order_with_totals, 2)
   end
 
   let(:orders) { Spree::Order.order("created_at") }
   let(:order) { orders.first }
 
-  describe "self.generate_csv" do
-    let(:csv) { orders.generate_csv }
+  describe "self.export_csv" do
+    let(:csv) { orders.export_csv }
+    subject { csv }
 
-    it "#generate_csv" do
-      csv.should include("id,number,total,state,shipment_state,email,user_id,completed_at,payment_state,special_instructions,firstname,lastname,address1,address2,city,zipcode,phone,state_name,alternative_phone,company")
-      csv.should include(order.id.to_s)
-      csv.should include(order.number)
-      csv.should include(order.total.to_s)
-      csv.should include(order.state)
-      csv.should include(order.shipment_state)
-      csv.should include(order.email)
-      csv.should include(order.user_id.to_s)
-      csv.should include(order.completed_at.to_s)
-      csv.should include(order.payment_state)
-      csv.should include(order.special_instructions) if order.special_instructions.present?
-      csv.should include(order.ship_address.firstname)
-      csv.should include(order.ship_address.lastname)
-      csv.should include(order.ship_address.address1)
-      csv.should include(order.ship_address.address2)
-      csv.should include(order.ship_address.city)
-      csv.should include(order.ship_address.zipcode)
-      csv.should include(order.ship_address.phone)
-      csv.should include(order.ship_address.state_name) if order.ship_address.state_name.present?
-      csv.should include(order.ship_address.alternative_phone)
-      csv.should include(order.ship_address.company)
-    end
+    it { should include("id,number,total,state,shipment_state,email,user_id,completed_at,payment_state,special_instructions,firstname,lastname,address1,address2,city,zipcode,phone,state_name,alternative_phone,company") }
+    it { should include(order.id.to_s) }
+    it { should include(order.number) }
+    it { should include(order.total.to_s) }
+    it { should include(order.state) }
+    it { should include(order.shipment_state) }
+    it { should include(order.email) }
+    it { should include(order.user_id.to_s) }
+    it { should include(order.completed_at.to_s) }
+    it { should include(order.payment_state) }
+    it { should include(order.special_instructions) if order.special_instructions.present? }
+    it { should include(order.ship_address.firstname) }
+    it { should include(order.ship_address.lastname) }
+    it { should include(order.ship_address.address1) }
+    it { should include(order.ship_address.address2) }
+    it { should include(order.ship_address.city) }
+    it { should include(order.ship_address.zipcode) }
+    it { should include(order.ship_address.phone) }
+    it { should include(order.ship_address.state_name) if order.ship_address.state_name.present? }
+    it { should include(order.ship_address.alternative_phone) }
+    it { should include(order.ship_address.company) }
   end
 
 end
