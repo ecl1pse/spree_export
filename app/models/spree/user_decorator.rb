348b7f20ddddb5c131bfb948dadb4a2618b73c80
Keitaroh Kobayashi <keita@kkob.us>
2013-08-29 13:49:56 +0900

    support for custom blocks to generate custom CSV content

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -5,12 +5,16 @@ Spree::User.instance_eval do
     options = SpreeExport.csv_options.merge options
 
     CSV.generate(options) do |csv|
-      user_column_names = ["id", "email", "login"]
-      column_headers = user_column_names
-      csv << column_headers
-      all.each do |user|
-        values = user.attributes.values_at(*user_column_names)
-        csv << values
+      if SpreeExport.user_csv_block.nil?
+        user_column_names = ["id", "email", "login"]
+        column_headers = user_column_names
+        csv << column_headers
+        all.each do |user|
+          values = user.attributes.values_at(*user_column_names)
+          csv << values
+        end
+      else
+        SpreeExport.user_csv_block.call csv, all
       end
     end
   end
--- lib/spree_export.rb
+++ lib/spree_export.rb
@@ -9,6 +9,9 @@ module SpreeExport
   mattr_accessor :output_encoding
   self.output_encoding = Encoding::UTF_8
 
+  mattr_accessor :order_csv_block
+  mattr_accessor :user_csv_block
+
   def self.config &block
     yield(self)
   end
