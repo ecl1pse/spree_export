c1aa154dd39a7014094473489227bab421762492
Keitaroh Kobayashi <keita@kkob.us>
2013-08-29 10:29:31 +0900

    Uses Spree.t to enable translation for the buttons

--- app/overrides/add_export_users_link.rb
+++ app/overrides/add_export_users_link.rb
@@ -3,7 +3,7 @@ Deface::Override.new(
   :name => 'add_export_users_csv_link',
   :insert_top => "#listing_users",
   :text => "
-    <%= link_to 'Export Users', spree.export_users_csv_path, class: 'button', id: 'export_csv' %>
+    <%= link_to Spree.t('export.export_users', :default =>'Export Users'), spree.export_users_csv_path, class: 'button', id: 'export_csv' %>
   ")
 
 
