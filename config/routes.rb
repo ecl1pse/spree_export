059206e07e32359a448a6ed01126acde36300274
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-09-27 03:56:47 -0700

    rails 4 support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.draw do
-  get '/admin/users_export_csv/', to: 'Admin::Users#export_csv', as: "export_users_csv"
-  get '/admin/orders/export_csv/', to: 'Admin::Orders#export_csv', as: "export_csv"
+  get '/admin/users_export_csv/', to: 'admin/users#export_csv', as: "export_users_csv"
+  get '/admin/orders/export_csv/', to: 'admin/orders#export_csv', as: "export_csv"
 end
